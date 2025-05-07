aa899f00f23339907e71c55e0bd2ff8e6f3d5ccc
Prathamesh Patil <94956959+Prath7057@users.noreply.github.com>
2025-05-07 23:05:07 +0530

    Update School.php

--- app/Models/School.php
+++ app/Models/School.php
@@ -12,27 +12,28 @@ use App\Notifications\SchoolVerifyEmailNotification;
 class School extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
-
+    //
     protected $fillable = ['name', 'address', 'email', 'phone', 'password', 'profile_picture', 'email_verified_at'];
-
+    //
     protected $hidden = ['password'];
-
+    //
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
+    //
     public function students()
     {
         return $this->hasMany(Student::class);
     }
-
+    //
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new SchoolResetPasswordNotification($token));
     }
-
+    //
     public function sendEmailVerificationNotification()
     {
         $this->notify(new SchoolVerifyEmailNotification());
     }
+    //
 }
